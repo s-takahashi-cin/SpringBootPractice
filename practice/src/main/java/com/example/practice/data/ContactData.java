74638a84500c0cc7b99f85abe9ae4f9bd792bcc4
s-takahashi-cin <s.takahashi.cingroup2012@gmail.com>
2024-03-24 22:38:02 -0700

    commit contact form step2

---
+++ practice/src/main/java/com/example/practice/data/ContactData.java
@@ -0,0 +1,16 @@
+package com.example.practice.data;
+
+import lombok.Data;
+
+@Data
+public class ContactData {
+    private String lastName;
+    private String firstName;
+    private String email;
+    private String phone;
+    private String zipCode;
+    private String address;
+    private String buildingName;
+    private String contactType;
+    private String body;
+}
